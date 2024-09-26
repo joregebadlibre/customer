47f9631553a0793f340b6d1c21a28798eb773240
joregebadlibre <joregebadlibre@hotmail.com>
2024-09-25 23:13:53 -0500

    Update Docker RabbitMW Serialize object

--- src/main/java/com/prueba/customer/service/impl/MessageConsumer.java
+++ src/main/java/com/prueba/customer/service/impl/MessageConsumer.java
@@ -1,5 +1,6 @@
 package com.prueba.customer.service.impl;
 
+import com.prueba.customer.repository.entity.Movimientos;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class MessageConsumer {
 
     @RabbitListener(queues = "myQueue")
-    public void handleMessage(String message) {
-        System.out.println("Received message: " + message);
+    public void handleMessage(Movimientos movimientos) {
+        System.out.println("Tu nuevo Saldo es: : " + movimientos.getSaldo());
     }
 }

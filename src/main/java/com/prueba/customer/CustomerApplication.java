51bd64ee7318df7a2e9d0a549cd9b7362210bfaf
JorgeAlvarez <joregebadlibre@gmail.com>
2024-10-15 00:11:20 -0500

    Complete Environtment Config

--- src/main/java/com/prueba/customer/CustomerApplication.java
+++ src/main/java/com/prueba/customer/CustomerApplication.java
@@ -1,5 +1,6 @@
 package com.prueba.customer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class CustomerApplication {
 
 	public static void main(String[] args) {
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		System.setProperty("SPRING_RABBITMQ_HOST", dotenv.get("SPRING_RABBITMQ_HOST"));
+//		System.setProperty("SPRING_RABBITMQ_PORT", dotenv.get("SPRING_RABBITMQ_PORT"));
+
 		SpringApplication.run(CustomerApplication.class, args);
 	}
 
